e38c6d41a69d982669e44399d5a23c2d8b32b578
Obi Uchenna David <daviduchenna@outlook.com>
2017-06-21 09:15:46 +0100

    fixed comments

--- edondome/settings.py
+++ edondome/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%#28eszcedox(07bqb0w(opckl&fx+ac*)ypc%9b&po5ib07od'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ENV = os.environ
