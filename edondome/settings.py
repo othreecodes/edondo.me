e4894e58c4f95e83753ec19b12074d3acf8c7687
Obi Uchenna David <daviduchenna@outlook.com>
2018-10-11 15:02:39 +0100

    upgraded some files

--- edondome/settings.py
+++ edondome/settings.py
@@ -82,24 +82,24 @@ WSGI_APPLICATION = 'edondome.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 #
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': ENV['DB_NAME'],
-        'USER': ENV['DB_USER'],
-        'PORT': ENV['DB_PORT'],
-        'PASSWORD': ENV['DB_PASS'],
-        'HOST': ENV['DB_HOST'],
-
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': ENV['DB_NAME'],
+#         'USER': ENV['DB_USER'],
+#         'PORT': ENV['DB_PORT'],
+#         'PASSWORD': ENV['DB_PASS'],
+#         'HOST': ENV['DB_HOST'],
+
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -167,3 +167,10 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
+
+# Force https redirect
+SECURE_SSL_REDIRECT = True
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
